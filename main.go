38a8db587f9315a456869df93270c024f48eed86
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2025-09-20 21:37:54 +0200

    optimize memory by putting out of scope variables when not needed anymore

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	var pcapFile = flag.String("pcap", "capture.pcap", "path to the pcap file")
 	var filter = flag.String("filter", "", "filter to use")
-	var verbose = flag.Bool("v", false, "verbose output")
+
 	var netifyApiKey = flag.String("netify-apikey", "", "Netify API key")
 	var netifyCacheServerPort = flag.Int("netify-cache-port", 3344, "Netify cache server port")
 	var netidyCacheFilesDir = flag.String("netify-cache-dir", "netify_cache", "Where cache Netify responses")
@@ -43,37 +43,6 @@ func main() {
 
 	if err := InitQuestDB("http://127.0.0.1:9000", experimentName); err != nil {
 		log.Fatalf("failed to init QuestDB: %v", err)
-	}
-
-	cmd := exec.Command(
-		"tshark",
-		"-r", *pcapFile,
-		"-Y", *filter,
-		"-T", "json",
-		"-e", "frame.time_epoch",
-		"-e", "ip.src",
-		"-e", "ip.dst",
-		"-e", "frame.len",
-		"-e", "ip.proto",
-		"-e", "tcp.port",
-		"-e", "udp.port",
-		"-e", "tls.handshake.extensions_server_name",
-		"-e", "tls.handshake.extensions_alpn_str",
-		"-e", "tcp.stream",
-		"-e", "udp.stream",
-	)
-
-	fmt.Fprintln(os.Stderr, cmd.String())
-
-	output, err := cmd.Output()
-	if err != nil {
-		log.Fatalf("failed to run tshark: %v", err)
-	}
-
-	// This can use a lot of memory depending on the number of packets
-	var tsPackets []TsharkPacket
-	if err := json.Unmarshal(output, &tsPackets); err != nil {
-		log.Fatalf("failed to parse JSON: %v", err)
 	}
 
 	if len(*netifyApiKey) == 0 {
@@ -97,37 +66,29 @@ func main() {
 		}
 	}()
 
-	sniMap := make(map[IpProto]map[int]*TLS)
-	var pkts = make([]*Packet, 0, len(tsPackets))
-	for _, tsPkt := range tsPackets {
-		pkt, err := tsPkt.ToPacket()
-		if err != nil {
-			log.Fatalln(err)
-		}
+	cmd := exec.Command(
+		"tshark",
+		"-r", *pcapFile,
+		"-Y", *filter,
+		"-T", "json",
+		"-e", "frame.time_epoch",
+		"-e", "ip.src",
+		"-e", "ip.dst",
+		"-e", "frame.len",
+		"-e", "ip.proto",
+		"-e", "tcp.port",
+		"-e", "udp.port",
+		"-e", "tls.handshake.extensions_server_name",
+		"-e", "tls.handshake.extensions_alpn_str",
+		"-e", "tcp.stream",
+		"-e", "udp.stream",
+	)
 
-		if _, ok := sniMap[pkt.IpProto]; !ok {
-			sniMap[pkt.IpProto] = make(map[int]*TLS)
-		}
+	fmt.Fprintln(os.Stderr, cmd.String())
 
-		if _, ok := sniMap[pkt.IpProto][pkt.StreamIndex]; !ok {
-			sniMap[pkt.IpProto][pkt.StreamIndex] = &TLS{UNKNOWN, UNKNOWN}
-		}
-
-		tls := sniMap[pkt.IpProto][pkt.StreamIndex]
-		if pkt.TLS != nil {
-			if len(pkt.TLS.Sni) != 0 {
-				tls.Sni = pkt.TLS.Sni
-			}
-			if len(pkt.TLS.Alpn) != 0 {
-				tls.Alpn = pkt.TLS.Alpn
-			}
-		}
-		pkt.TLS = tls
-
-		pkts = append(pkts, pkt)
-		if *verbose {
-			log.Debugln(pkt)
-		}
+	pkts, err := getPackets(cmd)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	log.Printf("found %d pkts", len(pkts))
@@ -173,4 +134,59 @@ func main() {
 	}
 
 	log.Infof("saved pkts in QuestDB on table: %s", GetTableName(experimentName))
+}
+
+func getTsharkPackets(cmd *exec.Cmd) ([]TsharkPacket, error) {
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("failed to run tshark: %v", err)
+	}
+
+	// This can use a lot of memory depending on the number of packets
+	var tsPackets []TsharkPacket
+	if err := json.Unmarshal(output, &tsPackets); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %v", err)
+	}
+
+	return tsPackets, nil
+}
+
+//goland:noinspection D
+func getPackets(cmd *exec.Cmd) ([]*Packet, error) {
+	tsPackets, err := getTsharkPackets(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	sniMap := make(map[IpProto]map[int]*TLS)
+	var pkts = make([]*Packet, 0, len(tsPackets))
+	for _, tsPkt := range tsPackets {
+		pkt, err := tsPkt.ToPacket()
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := sniMap[pkt.IpProto]; !ok {
+			sniMap[pkt.IpProto] = make(map[int]*TLS)
+		}
+
+		if _, ok := sniMap[pkt.IpProto][pkt.StreamIndex]; !ok {
+			sniMap[pkt.IpProto][pkt.StreamIndex] = &TLS{UNKNOWN, UNKNOWN}
+		}
+
+		tls := sniMap[pkt.IpProto][pkt.StreamIndex]
+		if pkt.TLS != nil {
+			if len(pkt.TLS.Sni) != 0 {
+				tls.Sni = pkt.TLS.Sni
+			}
+			if len(pkt.TLS.Alpn) != 0 {
+				tls.Alpn = pkt.TLS.Alpn
+			}
+		}
+		pkt.TLS = tls
+
+		pkts = append(pkts, pkt)
+	}
+
+	return pkts, nil
 }
